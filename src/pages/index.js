197528b7626c3bdb023f4d4b6cde79882ecee141
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2018-09-11 13:25:53 -0500

    lalit first gatsby commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi LALIT JAGTAP </h1>
+    <p>Welcome to your new 1st Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
